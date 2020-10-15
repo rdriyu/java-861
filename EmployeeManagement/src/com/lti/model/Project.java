60aa284b546eee8a943101d1c81d3278f15b0205
Riya D <Riya D@DESKTOP-M1DKFQU>
2020-10-15 13:12:27 +0530

    Added Project class

---
+++ EmployeeManagement/src/com/lti/model/Project.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Project {
+	int projectcode;
+	String projectName;
+	public int getProjectcode() {
+		return projectcode;
+	}
+	public void setProjectcode(int projectcode) {
+		this.projectcode = projectcode;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+}
