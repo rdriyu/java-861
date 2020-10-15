3742264be81d7b94de4a88bc395479219f3f8052
Riya D <Riya D@DESKTOP-M1DKFQU>
2020-10-15 13:25:57 +0530

    Added Department class

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Department {
+	int deptId;
+	String DeptName;
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public String getDeptName() {
+		return DeptName;
+	}
+	public void setDeptName(String deptName) {
+		DeptName = deptName;
+	}
+
+}
